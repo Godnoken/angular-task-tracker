53c881d11c9e7979b881830cac33f286040c39c1
Sebastian Ojefors <godnoken@gmail.com>
2022-03-23 20:17:54 +0100

    Incorporate task service in task component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { TASKS } from 'src/app/mock-task-data';
 import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -9,11 +9,14 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService
+  ) { }
 
   ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
   }
 
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { TASKS } from 'src/app/mock-task-data';
+import { Task } from 'src/app/Task';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
   constructor() { }
+
+  getTasks(): Task[] {
+    return TASKS;
+  }
 }
