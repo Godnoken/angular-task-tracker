ec98324db70bf2d3aa831c0044d818d8f226592d
Sebastian Ojefors <godnoken@gmail.com>
2022-03-23 19:21:23 +0100

    Add Task interface and mock-data

---
+++ src/app/mock-task-data.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        title: "Gym with Haydn",
+        date: "10th October at 5pm",
+        description: "",
+    },
+    {
+        id: 2,
+        title: "Revision school work",
+        date: "10th October at 10pm",
+        description: "Programming priority, Spanish if possible",
+    },
+    {
+        id: 3,
+        title: "Icehockey",
+        date: "12th October at 9:30pm",
+        description: "Hemsehallen, don't forget the stick",
+    },
+    {
+        id: 4,
+        title: "Prepare for birthday",
+        date: "13th October",
+        description: "Buy food. Buy invite cards and send them out",
+    },
+]
