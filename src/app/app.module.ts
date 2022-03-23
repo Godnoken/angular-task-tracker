e430ac26e3e7169edd18d84d4bdbf1487eaa467e
Sebastian Ojefors <godnoken@gmail.com>
2022-03-23 18:43:33 +0100

    Add all components & services needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AboutComponent } from './components/about/about.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AboutComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule
