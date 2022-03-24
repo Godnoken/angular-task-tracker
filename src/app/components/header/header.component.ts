2ed7c3718e930930b2a832a96ed831a5ff43088f
Sebastian Ojefors <godnoken@gmail.com>
2022-03-24 15:11:46 +0100

    Add method to open/close add task container

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   title = "TaskTracks";
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer2
+  ) { }
 
   ngOnInit(): void {
   }
 
+  handleAddTaskVisibility(): void {
+    const addTaskButton = this.renderer.selectRootElement(".add-task-button", true);
+    const addTaskContainer = this.renderer.selectRootElement(".add-task-container", true);
+
+    if (addTaskContainer?.classList.contains("active")) {
+      this.renderer.removeClass(addTaskContainer, "active");
+      this.renderer.setStyle(addTaskContainer, "display", "none");
+      this.renderer.setProperty(addTaskButton, "textContent", "Add");
+    }
+    else {
+      this.renderer.addClass(addTaskContainer, "active");
+      this.renderer.setStyle(addTaskContainer, "display", "block");
+      this.renderer.setProperty(addTaskButton, "textContent", "Close");
+    }
+  }
 }
